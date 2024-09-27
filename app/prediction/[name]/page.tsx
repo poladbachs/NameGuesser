9c46e05b1c8e857595699f7b7b419aa3e324e3af
Polad <bakhip@usi.ch>
2024-09-27 17:23:32 +0200

    predicted values are all set and working

--- app/prediction/[name]/page.tsx
+++ app/prediction/[name]/page.tsx
@@ -24,8 +24,14 @@ export default async function Page({ params }: Params) {
       countryData
     ]);
     return (
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"> 
-        { params.name }
-      </main>
+      <div>
+        <div>
+          <div>Personal Info</div>
+          <div>Name: {params.name}</div>
+          <div>Gender: {gender?.gender}</div>
+          <div>Country: {country?.country[0].country_id} {}</div>
+          <div>Occurencies: {gender?.count}</div>
+        </div>
+      </div>
     );
   }  
