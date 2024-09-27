ac1a5daaa9a65a064af0534b5e6dabd53aa31e70
Polad <bakhip@usi.ch>
2024-09-27 18:26:47 +0200

    Tailwind CSS for prediction results page, containers and title done

--- app/prediction/[name]/page.tsx
+++ app/prediction/[name]/page.tsx
@@ -24,9 +24,11 @@ export default async function Page({ params }: Params) {
       countryData
     ]);
     return (
-      <div>
-        <div>
-          <div>Personal Info</div>
+      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3 p-4">
+        <div className="p-8">
+          <div className="uppercase tracking-wide text-sm text-orange-500 font-semibold">
+            Personal Info
+          </div>
           <div>Name: {params.name}</div>
           <div>Gender: {gender?.gender}</div>
           <div>Country: {country?.country[0].country_id} {}</div>
