753bf3098f138b5f14db1e91921f679c2e3db023
Polad <bakhip@usi.ch>
2024-09-27 18:13:58 +0200

    refactored divs

--- app/page.tsx
+++ app/page.tsx
@@ -17,21 +17,21 @@ export default function Home() {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="p-4 shadow-md bg-white rounded-md">
         <h1 className="text-2xl font-semibold mb-4 text-black">Enter Your Name</h1>
+        <form onSubmit={handleSubmit} className="space-y-3">
+          <input 
+            type="text" 
+            placeholder="Type your name..." 
+            value={inputVal}
+            onChange={(e) => setInputval(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded text-black"
+          />
+          <button 
+            type="submit"
+            className="w-full py-2 px-4 bg-orange-500 hover:bg-orange-600 text-black font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+            >
+            Predict Identity</button>
+        </form>
       </div>
-      <form onSubmit={handleSubmit} className="space-y-3">
-        <input 
-          type="text" 
-          placeholder="Type your name..." 
-          value={inputVal}
-          onChange={(e) => setInputval(e.target.value)}
-          className="w-full p-2 border border-gray-300 rounded text-black"
-        />
-        <button 
-          type="submit"
-          className="w-full py-2 px-4 bg-orange-500 hover:bg-orange-600 text-black font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-          >
-          Predict Identity</button>
-      </form>
     </div>
   );
 }
