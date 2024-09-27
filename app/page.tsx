75b0e98824303e9411dd68f091f8bb994aca2900
Polad <bakhip@usi.ch>
2024-09-27 15:35:58 +0200

    added basic form to give input name and send data to APIs

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,10 @@ export default function Home() {
       <div>
         <h1>Enter Your Name</h1>
       </div>
+      <form>
+        <input type="text" placeholder="Type your name..."/>
+        <button type="submit">Predict Data</button>
+      </form>
     </div>
   );
 }
