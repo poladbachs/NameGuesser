3dfa018d220c93dff42b7f6c60e542e319612fd8
Polad <bakhip@usi.ch>
2024-09-27 15:48:40 +0200

    adding handleSubmit on form submit

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,16 @@ import { useState } from "react";
 
 export default function Home() {
   const [inputVal, setInputval] = useState("");
+
+  const handleSubmit = () => {
+
+  }
   return ( 
     <div>
       <div>
         <h1>Enter Your Name</h1>
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input 
           type="text" 
           placeholder="Type your name..." 
