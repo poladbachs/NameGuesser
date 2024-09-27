847ea480b00c9b7a9a921b3cfb1b7ac2a98e00c0
Polad <bakhip@usi.ch>
2024-09-27 17:53:16 +0200

    writing Tailwind CSS for form

--- app/page.tsx
+++ app/page.tsx
@@ -18,14 +18,15 @@ export default function Home() {
       <div className="p-4 shadow-md bg-white rounded-md">
         <h1 className="text-2xl font-semibold mb-4 text-black">Enter Your Name</h1>
       </div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="space-y-3">
         <input 
           type="text" 
           placeholder="Type your name..." 
           value={inputVal}
           onChange={(e) => setInputval(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded text-black"
         />
-        <button type="submit">Predict Data</button>
+        <button type="submit">Predict Identity</button>
       </form>
     </div>
   );
