d26bcbbca1bd8e56e5366ef824523e2e467255d0
Polad <bakhip@usi.ch>
2024-09-27 16:24:58 +0200

    interface typescript params type added

--- app/prediction/[name]/page.tsx
+++ app/prediction/[name]/page.tsx
@@ -1,6 +1,9 @@
+interface Params {
+  params: { name: string };
+}
 
 
-export default function Page({ params }: any) {
+export default function Page({ params }: Params) {
     return (
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"> 
         { params.name }
